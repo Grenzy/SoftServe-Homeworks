b38d49d42317996ba8fbec020628f05433b83794
Evgeniy Velegura <evgeniy.velegura@gmail.com>
2019-01-07 12:49:03 +0200

    Create project Homework_4

---
+++ Homework_4/Homework_4.BLL/Application.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4.BLL
+{
+    public class Application
+    {
+        public void Start(params string[] args)
+        {
+
+        }
+    }
+}
---
+++ Homework_4/Homework_4.BLL/SearchService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_4.BLL
+{
+    class SearchService
+    {
+        public static IList<int>FF (string pattern)
+        {
+            return null;
+        }
+    }
+}
