c18653fbc093ba2d9cc9873378ce4b5d1edd456d
Evgeniy Velegura <evgeniy.velegura@gmail.com>
2019-01-14 00:01:30 +0200

    Refactoring ConsoleUI

--- Task_4_FindReplaceApp/Task_4_FindReplaceApp.CUI/ConsoleUI.cs
+++ Task_4_FindReplaceApp/Task_4_FindReplaceApp.CUI/ConsoleUI.cs
@@ -8,87 +8,84 @@ namespace Task_4_FindReplaceApp.CUI
 {
     public class ConsoleUI
     {
-        public static void ShowSearchedItems(IList<SearchedItemDTO> si)
+        private const int CONSOLE_LENGTH = 120;
+        private static readonly string separator = new String('-', CONSOLE_LENGTH);
+        public static void ShowSearchedItems(IList<SearchedItemDTO> searchedItems)
         {
-            for (int i = 0; i < si.Count; i++)
+            for (int i = 0; i < searchedItems.Count; i++)
             {
-                Console.WriteLine(new String('-', 120));
-                if (si[i].PriviousLine != null)
+                if (searchedItems[i].PriviousLine != null)
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    Console.Write($"{si[i].PatternStringNumber - 1}.");
-                    Console.ResetColor();
-                    Console.WriteLine($"{si[i].PriviousLine}");
-                }
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.Write($"{si[i].PatternStringNumber}. ");
-                Console.ResetColor();
-                Console.Write($"{si[i].RightPart}");
-                Console.BackgroundColor = ConsoleColor.DarkGreen;
-                Console.Write($"{si[i].Pattern}");
-                Console.ResetColor();
-                Console.WriteLine($"{si[i].LeftPart}");
-                if (si[i].NextLine != null)
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    Console.Write($"{si[i].PatternStringNumber + 1}. ");
-                    Console.ResetColor();
-                    Console.WriteLine($"{si[i].NextLine}");
+                    ColorWrite($"{searchedItems[i].PatternStringNumber - 1}.", ConsoleColor.Black, ConsoleColor.DarkYellow);
+                    Console.WriteLine($"{searchedItems[i].PriviousLine}");
                 }
 
+                ColorWrite($"{searchedItems[i].PatternStringNumber}. ", ConsoleColor.Black, ConsoleColor.DarkYellow);
+                Console.Write($"{searchedItems[i].RightPart}");
+                ColorWrite($"{searchedItems[i].Pattern}", ConsoleColor.DarkGreen, ConsoleColor.White);
+                Console.WriteLine($"{searchedItems[i].LeftPart}");
+
+                if (searchedItems[i].NextLine != null)
+                {
+                    ColorWrite($"{searchedItems[i].PatternStringNumber + 1}. ", ConsoleColor.Black, ConsoleColor.DarkYellow);
+                    Console.WriteLine($"{searchedItems[i].NextLine}");
+                }
+                Console.WriteLine(separator);
             }
 
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine($"Number of line entries in a text file: {si.Count}");
+            string numberOfEntries = $"{Environment.NewLine}Number of line entries in a text file: {searchedItems.Count}";
+
+            Console.WriteLine(numberOfEntries);
         }
-        public static void ShowReplacedItems(IList<ReplacedItemDTO> ri)
+        public static void ShowReplacedItems(IList<ReplacedItemDTO> replacedItems)
         {
-            for (int i = 0; i < ri.Count; i++)
+            for (int i = 0; i < replacedItems.Count; i++)
             {
-                Console.WriteLine(new String('-', 120));
-                if (ri[i].PriviousLine != null)
+                if (replacedItems[i].PriviousLine != null)
                 {
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    Console.Write($"{ri[i].PatternStringNumber - 1}.");
-                    Console.ResetColor();
-                    Console.WriteLine($"{ri[i].PriviousLine}");
-                }
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.Write($"{ri[i].PatternStringNumber}. ");
-                Console.ResetColor();
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.Write($"{ri[i].RightPart}");
-                Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                Console.Write($"{ri[i].OldValue}");
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.WriteLine($"{ri[i].LeftPart}");
-                Console.ResetColor();
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.Write($"{ri[i].PatternStringNumber}. ");
-                Console.ResetColor();
-                Console.BackgroundColor = ConsoleColor.DarkGreen;
-                Console.Write($"{ri[i].RightPart}");
-                Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                Console.Write($"{ri[i].NewValue}");
-                Console.BackgroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine($"{ri[i].LeftPart}");
-                Console.ResetColor();
-                if (ri[i].NextLine != null)
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                    Console.Write($"{ri[i].PatternStringNumber + 1}. ");
-                    Console.ResetColor();
-                    Console.WriteLine($"{ri[i].NextLine}");
+                    ColorWrite($"{replacedItems[i].PatternStringNumber - 1}.", ConsoleColor.Black, ConsoleColor.DarkYellow);
+                    Console.WriteLine($"{replacedItems[i].PriviousLine}");
                 }
 
+                ColorWrite($"{replacedItems[i].PatternStringNumber}. ", ConsoleColor.Black, ConsoleColor.DarkYellow);
+                ColorWrite($"{replacedItems[i].RightPart}", ConsoleColor.Red, ConsoleColor.White);
+                ColorWrite($"{replacedItems[i].OldValue}", ConsoleColor.DarkMagenta, ConsoleColor.White);
+                ColorWriteLine($"{replacedItems[i].LeftPart}", ConsoleColor.Red, ConsoleColor.White);
+
+                ColorWrite($"{replacedItems[i].PatternStringNumber}. ", ConsoleColor.Black, ConsoleColor.DarkYellow);
+                ColorWrite($"{replacedItems[i].RightPart}", ConsoleColor.DarkGreen, ConsoleColor.White);
+                ColorWrite($"{replacedItems[i].NewValue}", ConsoleColor.DarkMagenta, ConsoleColor.White);
+                ColorWriteLine($"{replacedItems[i].LeftPart}", ConsoleColor.DarkGreen, ConsoleColor.White);
+
+                if (replacedItems[i].NextLine != null)
+                {
+                    ColorWrite($"{replacedItems[i].PatternStringNumber + 1}. ", ConsoleColor.Black, ConsoleColor.DarkYellow);
+                    Console.WriteLine($"{replacedItems[i].NextLine}");
+                }
+                Console.WriteLine(separator);
             }
 
-            Console.WriteLine(Environment.NewLine);
-            Console.WriteLine($"Number of replacements: {ri.Count}");
+            string NumberOfReplacements = $"{Environment.NewLine}Number of replacements: {replacedItems.Count}";
+
+            Console.WriteLine(NumberOfReplacements);
         }
         public static void ShowHelp()
         {
             Console.WriteLine("help");
+        }
+        private static void ColorWriteLine(string text, ConsoleColor backgraubdColor, ConsoleColor foregroundColor)
+        {
+            Console.BackgroundColor = backgraubdColor;
+            Console.ForegroundColor = foregroundColor;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+        private static void ColorWrite(string text, ConsoleColor backgraubdColor, ConsoleColor foregroundColor)
+        {
+            Console.BackgroundColor = backgraubdColor;
+            Console.ForegroundColor = foregroundColor;
+            Console.Write(text);
+            Console.ResetColor();
         }
     }
 }
