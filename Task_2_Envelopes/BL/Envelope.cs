14db1416ce310dfc7f0adc56ecb438398b66d0dd
Evgeniy Velegura <evgeniy.velegura@gmail.com>
2019-01-22 01:43:57 +0200

    Add task 2

---
+++ Task_2_Envelopes/BL/Envelope.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_2_Envelopes.BL.Interfaces;
+
+namespace Task_2_Envelopes.BL
+{
+    class Envelope : INestable<Envelope>
+    {
+        private readonly double sideA;
+        private readonly double sideB;
+
+        public double SideA => sideA;
+        public double SideB => sideB;
+
+        public bool IsNested(Envelope other)
+        {
+            return ((sideA > other.SideA) && (sideB > other.sideB)) ||
+                ((sideA > other.sideB) && sideB > other.SideA);
+        }
+    }
+}
---
+++ Task_2_Envelopes/BL/Interfaces/INestable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_Envelopes.BL.Interfaces
+{
+    internal interface INestable<T>
+    {
+        bool IsNested(T other);
+    }
+}
