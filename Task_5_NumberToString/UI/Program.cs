b860c903a3d2daa137e58a213ce5fe8c4910f988
Evgeniy Velegura <evgeniy.velegura@gmail.com>
2019-01-25 23:37:30 +0200

    Create task 5 project

---
+++ Task_5_NumberToString/UI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5_NumberToString.UI
+{
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            new Application().Start(args);
+        }
+    }
+}
