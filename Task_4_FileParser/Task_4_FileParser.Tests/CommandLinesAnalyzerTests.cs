a7f2ac4e50bb9f60bf8febf0ba68532f5045acad
Evgeniy Velegura <evgeniy.velegura@gmail.com>
2019-01-30 12:56:43 +0200

    Add tests for task 4

--- Task_4_FileParser/Task_4_FileParser.Tests/CommandLinesAnalyzerTests.cs
+++ Task_4_FileParser/Task_4_FileParser.Tests/CommandLinesAnalyzerTests.cs
@@ -9,8 +9,15 @@ namespace Task_4_FileParser.Tests
     [TestFixture]
     class CommandLinesAnalyzerTests
     {
-        [TestCase(new string[] { "test.txt", "word", "-i" }, Feature.Find, 
+        [TestCase(new string[] { "test.txt", "word", "-i" }, Feature.Find,
             "test.txt", "word", "", true)]
+        [TestCase(new string[] { "C:\\test.txt", "word", "-n" }, Feature.Find,
+            "C:\\test.txt", "word", "", false)]
+        [TestCase(new string[] { "C:\\test.txt", "word", "anotherWord", "-n" },
+            Feature.Replace, "C:\\test.txt", "word", "anotherWord", false)]
+        [TestCase(new string[] { "C:\\qqq\\test.txt", "word", "anotherWord", "-i" },
+            Feature.Replace, "C:\\qqq\\test.txt", "word", "anotherWord", true)]
+
         public void ParseArgs_ValidArguments_ReturnValidResult(string[] args,
             Feature expectedFeature, string expectedPath,
             string expectedPattern, string expectedNewValue,
@@ -20,7 +27,7 @@ namespace Task_4_FileParser.Tests
             string resultPattern;
             string resultNewValue;
             bool resultIgnoreCase;
-            Feature resultFeature = CommandLinesAnalyzer.ParseArgs(args, 
+            Feature resultFeature = CommandLinesAnalyzer.ParseArgs(args,
                 out resultPath, out resultPattern, out resultNewValue,
                 out resultIgnoreCase);
 
@@ -31,5 +38,44 @@ namespace Task_4_FileParser.Tests
             Assert.AreEqual(expectedIgnoreCase, resultIgnoreCase);
         }
 
+        [Test]
+        public void ParseArgs_InvalidKeyArgument_ReturnsException()
+        {
+            string[] args = { "C:\\test.txt", "word", "anotherWord", "-d" };
+            string resultPath;
+            string resultPattern;
+            string resultNewValue;
+            bool resultIgnoreCase;
+
+            Assert.Catch<ArgumentException>(() => CommandLinesAnalyzer.ParseArgs(
+                args, out resultPath, out resultPattern, out resultNewValue,
+                out resultIgnoreCase));
+        }
+        [Test]
+        public void ParseArgs_LackOfArguments_ReturnsException()
+        {
+            string[] args = { "C:\\test.txt", "word" };
+            string resultPath;
+            string resultPattern;
+            string resultNewValue;
+            bool resultIgnoreCase;
+
+            Assert.Catch<ArgumentException>(() => CommandLinesAnalyzer.ParseArgs(
+                args, out resultPath, out resultPattern, out resultNewValue,
+                out resultIgnoreCase));
+        }
+        [Test]
+        public void ParseArgs_ExtraArguments_ReturnsException()
+        {
+            string[] args = { "C:\\test.txt", "word", "anotherWord", "-d", "extraString" };
+            string resultPath;
+            string resultPattern;
+            string resultNewValue;
+            bool resultIgnoreCase;
+
+            Assert.Catch<ArgumentException>(() => CommandLinesAnalyzer.ParseArgs(
+                args, out resultPath, out resultPattern, out resultNewValue,
+                out resultIgnoreCase));
+        }
     }
 }
