bb74d57abdf260d903b5f33e493b40ec3dd576e1
Evgeniy Velegura <evgeniy.velegura@gmail.com>
2019-01-30 02:27:00 +0200

    Add test for task 4

---
+++ Task_4_FileParser/Task_4_FileParser.Tests/StringSplitterTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Task_4_FileParser.BL;
+
+namespace Task_4_FileParser.Tests
+{
+    [TestFixture]
+    class StringSplitterTests
+    {
+        [TestCase("anababebaer", new int[] { 3, 7 }, 2, new string[] { "ana", "ba", "be", "ba", "er" })]
+        public void SplitByIndexes_ValidParams_ReturnValidResult(string source,
+            int[] indexes, int length, string[] expected)
+        {
+            StringSplitter stringSplitter = new StringSplitter();
+
+            string[] result = stringSplitter.SplitByIndexes(source, indexes, length);
+
+            CollectionAssert.AreEqual(result, expected);
+        }
+    }
+}
